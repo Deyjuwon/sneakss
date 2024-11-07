d178b0f54656d34d4cda33b96c965320ee1b2130
Deyjuwon <temitayoadejuwon@gmail.com>
2024-11-07 18:27:33 +0100

    clean up

--- src/components/DiscountSection.jsx
+++ src/components/DiscountSection.jsx
@@ -1,12 +1,15 @@
+import { useNavigate } from "react-router-dom"
 import Button from "./Button"
 
 
 const DiscountSection = () => {
+  const navigate = useNavigate()
+
   return (
     <section className="h-72 flex flex-col gap-4 items-center justify-center text-white bg-primaryYellow">
         <h2 className="font-bold text-xl text-center md:text-4xl">GET A 10% DISCOUNT ON ALL PRODUCTS EVERY WEDNESDAY!</h2>
-        <p className="font-semibold text-base  md:text-xl">Customer Happiness Fuels Our Success.</p>
-        <Button title='shop now' />
+        <p  className="font-semibold text-base  md:text-xl">Customer Happiness Fuels Our Success.</p>
+        <div onClick={() => navigate('/new')}><Button title='shop now'  /></div>
     </section>
   )
 }
