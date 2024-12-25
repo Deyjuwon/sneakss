783093b588de217f0ad5810e5c79c0f58af748a5
Deyjuwon <temitayoadejuwon@gmail.com>
2024-12-25 12:47:25 +0100

    bug fix

--- src/components/NewProductCard.jsx
+++ src/components/NewProductCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NewProductCard = ({ shoeName, shoeColor, shoeGender, shoePrice, shoeImg, ProductID }) => {
   return (
     <Link to={`/product/${ProductID}`}>
-      <div className="p-0 md:p-4 cursor-pointer min-h-80 flex flex-col gap-1 md:gap-3 text-sm border border-transparent hover:border-primaryRed  md:w-fit">
+      <div className="p-2 md:p-4 cursor-pointer md:min-h-80 flex flex-col gap-1 md:gap-3 text-sm border border-transparent hover:border-primaryRed w-full md:w-fit">
         <img className="h-[170px] min-w-[150px] md:h-52 md:min-w-56 object-cover" src={shoeImg} alt="" />
         <p className="font-bold">{shoeName}</p>
         <div className="flex flex-col gap-0">
